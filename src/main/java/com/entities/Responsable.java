1ba01aa487b4e8979be2a8121f9f6fe83df58572
Chomp <chomphoo@protonmail.com>
2022-03-06 23:43:57 +0100

    Create entity responsable

---
+++ src/main/java/com/entities/Responsable.java
@@ -0,0 +1,14 @@
+package com.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Responsable {
+    @Id
+    private String login;
+
+    @OneToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "login")
+    @MapsId
+    private Utilisateur utilisateur;
+}
